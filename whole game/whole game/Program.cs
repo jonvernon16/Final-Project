b51649e1691a0ee2fbb3a9893b2ef231cf52c51a
jonvernon16 <57112508+jonvernon16@users.noreply.github.com>
2019-12-17 15:29:02 -0600

    made a new file called whole game. just trying to get some expereince before the actual game

---
+++ whole game/whole game/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace whole_game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] map = { 0, 1, 2, 3 };
+
+            string userEntry;
+            string north;
+            string east;
+            string south;
+            string west;
+
+            bool again = true;
+            Console.WriteLine("Spooky Forest\nPress any key to start.");
+            Console.ReadKey(true);
+            //Console.ReadKey waits for an input
+            Console.WriteLine("You awake in an empty clearing in a forest. \n"
+                + " There are 3 possible paths, North, East, or West.\n Which do you choose?\n");
+            userEntry = Console.ReadLine();
+            while (again)
+            {
+                string readKey;
+                readKey = Convert.ToString(Console.ReadKey(true));
+                if (readKey == "w")
+                {
+                    Console.WriteLine("Forward");
+                }
+
+                
+            }
+
+        }
+
+    }
+}
