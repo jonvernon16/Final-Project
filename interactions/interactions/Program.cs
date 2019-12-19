f090d0d5826106e5d70cacae557206f9afc34d70
jonvernon16 <57112508+jonvernon16@users.noreply.github.com>
2019-12-19 15:19:57 -0600

    figured out movment

---
+++ interactions/interactions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace interactions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ inventoryAndInteractions/inventoryAndInteractions/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace inventoryAndInteractions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userEntry;
+            string axe;
+            int[] inventory = { 0, 1, 2, 3, 4 };
+            Console.WriteLine("You see an axe, pick it up?");
+            userEntry = Console.ReadLine();
+
+            if (userEntry == "y")
+            {
+                Console.WriteLine("You picked up the axe.\nPress anything to continue...");
+                axe = Convert.ToString(inventory[0]);
+            }
+            else if (userEntry == "n")
+            {
+                Console.WriteLine("You ignored the axe");
+            }
+            Console.ReadKey();
+            Console.WriteLine("You see a tree");
+        }
+        public static void pickUp()
+        {
+
+        }
+    }
+}
