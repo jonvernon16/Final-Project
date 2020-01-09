2e4446ba1a25accef9a3f56353407183d028c171
jonvernon16 <57112508+jonvernon16@users.noreply.github.com>
2020-01-09 15:28:50 -0600

    figured out inventory, still working on "use"

--- interactions/interactions/Program.cs
+++ interactions/interactions/Program.cs
@@ -6,7 +6,64 @@ namespace interactions
     {
         static void Main(string[] args)
         {
-            
+            //create an iventory system, you do not have to use arrays I think.
+            bool axe = false;
+            bool ball = false;
+            bool knife = false;
+            bool gameOn = true; 
+
+            string userEntry;
+            string objectEntry;
+
+            while (gameOn == true)
+            {
+                Console.WriteLine("There is a ball on the ground in front of you, do you wish to pick it up?");
+                userEntry = Console.ReadLine();
+                if (userEntry == "yes")
+                { 
+                    Console.WriteLine("You picked up the ball.");
+                    ball = true;
+                }
+                else if (userEntry == "no")
+                {
+                    Console.WriteLine("You ignored the ball.");
+                }
+                Console.WriteLine("After you pick up the ball a door seems to appear out of the haze in front of you\n What will you do? ");
+                userEntry = Console.ReadLine();
+
+
+                checkInventory(userEntry, axe, ball, knife);
+                inspect(userEntry, objectEntry);
+            }
         }
+        public static void checkInventory(string userEntry, bool axe, bool ball, bool knife)
+        {
+            if (userEntry == "check inventory")
+            {
+                if (axe == true)
+                {
+                    Console.WriteLine("Axe\n");
+                }
+                else if (ball == true)
+                {
+                    Console.WriteLine("Ball\n");
+                }
+                else if (knife == true)
+                {
+                    Console.WriteLine("Knife\n");
+                }
+            }
+        }
+        public static void inspect(string objectEntry, string userEntry)
+        {
+            if (userEntry == "inspect " + objectEntry)
+            {
+                if (objectEntry == "door")
+                {
+                    Console.WriteLine("You look at the door closer, you see a small round hole on the side of the door.");
+                }
+            }
+        }
+           
     }
 }
